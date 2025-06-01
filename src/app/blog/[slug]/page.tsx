143f01ed0cbb98cc440871f0cd17f98048de7cc7
Mauricio <mauricio_cy@hotmail.com>
2025-06-01 18:18:21 -0300

    chore: add seo in blog content

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,10 +1,30 @@
 import { PostPage } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 type BlogPostPageProps = {
   params: Promise<{ slug: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) return {};
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: "index, follow",
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
 
 export const revalidate = 60;
 
