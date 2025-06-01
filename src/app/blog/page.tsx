1f4e420a5486f2d677bbcf2ec242c926b9d05a41
Mauricio <mauricio_cy@hotmail.com>
2025-06-01 18:05:39 -0300

    chore: add seo in blog list page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar seu negócio",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Dicas e estratégias para impulsionar seu negócio",
+    url: "https://site-blog-next15.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-next15.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
