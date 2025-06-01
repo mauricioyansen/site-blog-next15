08d0a1d49099649799bd855999cde44ea102910e
Mauricio <mauricio_cy@hotmail.com>
2025-06-01 17:58:53 -0300

    seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
 import { LandingPage } from "@/templates/landing-page";
 import { ErrorComponent } from "./component";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Site.Set",
+  description: "Venda seus produtos como afiliado em um único lugar",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.Set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "https://site-blog-next15.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-next15.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 
 export default function HomePage() {
   return <LandingPage />;
